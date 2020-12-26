9409910233eb60654d2fcde1ddfb7075f9bc1186
Harrf Akbar <harrf_akbar@yahoo.com>
2020-12-26 19:03:47 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,9 +2,9 @@ require 'pry'
 def join_nested_strings(src)
   new_array = []
   index_row = 0 
-  while index_row < src.length do
+  while index_row < src.count do
     element_index = 0 
-    while element_index > src[index_row].length do 
+    while element_index < src[index_row].count do 
       if src[index_row][element_index].class == String
         string_only = src[index_row][element_index]
       end
